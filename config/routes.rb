0a63b58cc909d882412ca7bbc94f1a8f19dad693
Abe <es335ab@gmail.com>
2014-12-27 13:00:53 +0900

    テンプレーエンジンをslimに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'hoge/index'
+  # get 'hoge/index'
+  resources :hoge
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
